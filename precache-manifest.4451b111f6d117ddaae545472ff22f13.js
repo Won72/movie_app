fe43c1af8b86410147da2cb59ac593a2a607feeb
Won72 <68599139+Won72@users.noreply.github.com>
2020-11-07 20:03:25 +0100

    Updates

---
+++ precache-manifest.4451b111f6d117ddaae545472ff22f13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0065f845c25d7ef400e459d2bfd67b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f3d0fe0cb2a6df2f3746",
+    "url": "/movie_app/static/css/main.adf50e78.chunk.css"
+  },
+  {
+    "revision": "fa5e0544dd32d30ca5a4",
+    "url": "/movie_app/static/js/2.9b6f4c02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.9b6f4c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d0fe0cb2a6df2f3746",
+    "url": "/movie_app/static/js/main.edf70926.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
